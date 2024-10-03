53ae885f637a08c7c0f2559a9431019777638920
Enner Mendizabal <159963896+Ennero@users.noreply.github.com>
2024-10-02 19:00:58 -0600

    Create interfaz.py

---
+++ Proyecto2/interfaz.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import PhotoImage
+from tkinter import filedialog
+
+
+
+#Aquí pego el código que voy a reutilizar del proyecto anterior
+
+
+
