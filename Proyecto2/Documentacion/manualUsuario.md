a6cc61affcb59452492b1c798483727d8029e1ec
Enner Mendizabal <159963896+Ennero@users.noreply.github.com>
2024-10-06 23:04:31 -0600

    Comenzando bien proyecto 2

---
+++ Proyecto2/Documentacion/manualUsuario.md
@@ -0,0 +1,26 @@
+# Manual de Usuario - Proyecto 2
+## Objetivos
+### General
+- Introducir facilmente al comportamiento y funcionamiento del programa
+### Especificos
+- Presentar el flujo del programa
+- Explicar las funcionalidades que provee el programa
+## Introducción
+Este manual se creó con la finalidad de ayudar al futuro usuario del programa a utilizarlo correctamente, pudiendo aprovechar cada una de las diversar funcionalidades que este programa informático proporciona, otorgando la información de cómo estas trabajan individual y conjuntamente estructurando claramente el flujo del programa y cómo ocurre su ejecución.
+
+
+Este programa, es una herramienta para creación de paginas web que tiene un área de edición código y un área en donde se presentarán todo los errores del programa. Este programa será capaz de leer archivos con extensión .LFP, los cuales contendrán intrucciones como: Controles, Propiedades y Colocación. Cada una de estas intrucciones que contiene el archivo permitirán el diseño de paginas web, generando código HTML y CSS.
+
+Si hay problemas con la sintaxis o los lexemas utilizados en el programa, este será capaz de listar cada uno de los errores e indicar la posición y el tipo de error que hubo. En caso de no haber ningun error, este generará los archivos, el HTML y el CSS, dentro de una carpeta a la que se podrá acceder posteriormente para visualizar la página.
+
+
+## Requisito del Sistema
+- Poseer el ejecutable
+- 1GB de memoria RAM
+- Procesador con Arquitectura x86
+- Sistema Operativo capaz de leer archivos con extensión .exe
+- Pantalla
+- Teclado (opcional)
+
+
+## Flujo de las funcionalidades del programa
--- Proyecto2/interfaz.py
+++ Proyecto2/interfaz.py
@@ -1,19 +1,40 @@
 import tkinter as tk
-from tkinter import messagebox
-from tkinter import PhotoImage
 from tkinter import filedialog
+from tkinter import messagebox
+from tkinter import ttk as tkk
+
+import subprocess
+
+errores=[]
 
 
 
 # Función para analizar el texto
 def analizar():
-    pass
+    #global pais, poblacion, rutaGrafica, rutaBandera
+    cuerpo=entrada.get("1.0", tk.END) #Obtengo el contenido del editor de texto
+    lineas=cuerpo.splitlines() #Divido el contenido del editor de texto por líneas
+    todasVacias=all(linea=="" or linea.isspace() for linea in lineas) #Verifico si todas las líneas estan vacias
+    if todasVacias:
+        messagebox.showerror("Error", "No se ha ingresado información.") #Mensaje de error
+        info.config(text="Ingrese Información en el editor de texto", foreground="red") #Mensaje de error
+    else:
+        info.config(text="Analizando...", foreground="black") #Mensaje que dice el proceso
 
+        datos=entrada.get("1.0","end-1c")
+        resultado=subprocess.run(['./main.exe'],input=datos, stdout=subprocess.PIPE,text=True)
+        #salida=resultado.stdout.strip()
+        print(resultado.stdout)
+        #Aquí coloco todo lo demás del análisis
+        
 
-def guardar(): #Función para guardar el archivo
+def actualizarTabla(): #Función para actualizar la tabla de errores
     pass
 
 def tokens(): #Función para mostrar los tokens
+
+
+
     pass
 
 
@@ -24,21 +45,24 @@ def tokens(): #Función para mostrar los tokens
 
 #Todo lo relacionado a la interfaz gráfica ----------------------------------------------
 
-#Declaro e inicializo las variables globales
+#Declaro e inicializo las variables globales para la interfaz
 guardado=False
 ruta=""
+preguntar=True
 
 # Función para la posición del mouse
 def posicion(event):
     x, y = event.x, event.y
     pos.config(text=f"x={x}, y={y}")
 
+
 def abro(): #Función para abrir un archivo
-    global ruta, guardado #Uso las variables globales
+    global ruta, guardado,preguntar #Uso las variables globales
     info.config(text="Abriendo archivo...", foreground="black") #Mensaje que dice el proceso
-    ruta=filedialog.askopenfilename(title="Abrir archivo", filetypes=(("Archivos .ORG", "*.org"),)) #Solo acepta archivos .org
+    ruta=filedialog.askopenfilename(title="Abrir archivo",filetypes=(("Archivos .ORG", "*.org"),)) #Solo acepta archivos .org
     if ruta != "": #Si la ruta no está vacía
         guardado=True #Cambio el estado de guardado
+        preguntar=False
         try:
             with open(ruta, "r", encoding="utf-8") as archivo: #Abro el archivo
                 cuerpo=archivo.read() #Leo el archivo
@@ -67,17 +91,29 @@ def guardar(): #Función para guardar el archivo
         guardarComo() #Si no se ha guardado, llamo a la función guardarComo
 
 def guardarComo(): #Función para guardar el archivo como
-    global ruta, guardado #Uso las variables globales
+    global ruta, guardado,preguntar #Uso las variables globales
     info.config(text="Guardando archivo como...", foreground="black") #Mensaje que dice el proceso
-    ruta=filedialog.asksaveasfilename(title="Guardar archivo")
+    ruta=filedialog.asksaveasfilename(title="Guardar archivo",filetypes=(("Archivos .LFP", "*.LFP"),))
     if ruta != "": #Si la ruta no está vacía
         guardado=True #Cambio el estado de guardado
         guardar() #Llamo a la función guardar
     else:
         info.config(text="No se guardó el archivo", foreground="red") #Mensaje de error
 
+def pregunta(): #Función para preguntar si se quiere guardar
+    global guardado, ruta, preguntar #Uso las variables globales
+    preguntando=messagebox.askyesno("Guardar", "¿Desea guardar los cambios?") #Pregunta si se quiere guardar
+    if preguntando: #Si se quiere guardar
+        guardar() #Llamo a la función guardar
+    else: #Si no se quiere guardar
+        info.config(text="No se guardó el archivo", foreground="red") #Mensaje de error
+
 def nuevar(): #Función para empezar un archivo nuevo
-    global ruta, guardado #Uso las variables globales
+    global ruta, guardado,preguntar #Uso las variables globales
+    if preguntar:
+        pregunta()
+
+    preguntar=True
     info.config(text="Creando nuevo archivo...", foreground="black") #Mensaje que dice el proceso
     ruta="" #La ruta se vuelve vacía
     guardado=False #El estado de guardado se vuelve falso
@@ -92,7 +128,7 @@ ventana.title("Proyecto 1 LFP")
 frame1 = tk.Frame(ventana)
 frame1.pack(side=tk.TOP)
 frame2 = tk.Frame(ventana)
-frame2.pack(side=tk.BOTTOM)
+frame2.pack(side=tk.BOTTOM, expand=True, fill="both")
 frame3 = tk.Frame(frame1)
 frame3.pack(side=tk.LEFT)
 frame4 = tk.Frame(frame1)
@@ -119,7 +155,7 @@ entrada.config(yscrollcommand=scroll.set)
 scroll.pack(side="right", fill="y")
 
 # Label que irá diciendo que está pasando en el programa
-info=tk.Label(frame1,text=" ")
+info=tk.Label(frame4,text=" ")
 info.pack()
 info.config(foreground="red", font=("Arial", 9, "italic"))
 
@@ -182,7 +218,50 @@ salir.pack(side="bottom")
 salir.config(background="white", foreground="red", font=("Arial", 13, "bold"))
 
 
-#Creación del botón para guardar como
+#Creación de la tabla de errores ----------------------------------------------
+arbol=tkk.Treeview(frame2, columns=("Tipo","Linea","Columna","Token","Descripción"), show="headings")
+arbol.pack(side="left") #Lo empaqueto en el frame2
+#Encabezados
+arbol.heading("Tipo", text="Tipo")
+arbol.heading("Linea", text="Linea")
+arbol.heading("Columna", text="Columna")
+arbol.heading("Token", text="Token")
+arbol.heading("Descripción", text="Descripción")
+
+#Datos de prueba
+data = [
+    ("Ana", 25, "Madrid", "España", "Femenino"),
+    ("Pedro", 30, "Barcelona", "España", "Masculino"),
+    ("Luisa", 22, "Sevilla", "España", "Femenino"),
+    ("Ana", 25, "Madrid", "España", "Femenino"),
+    ("Pedro", 30, "Barcelona", "España", "Masculino"),
+    ("Luisa", 22, "Sevilla", "España", "Femenino"),
+    ("Ana", 25, "Madrid", "España", "Femenino"),
+    ("Pedro", 30, "Barcelona", "España", "Masculino"),
+    ("Luisa", 22, "Sevilla", "España", "Femenino"),
+    ("Ana", 25, "Madrid", "España", "Femenino"),
+    ("Pedro", 30, "Barcelona", "España", "Masculino"),
+    ("Luisa", 22, "Sevilla", "España", "Femenino"),
+    ("Ana", 25, "Madrid", "España", "Femenino"),
+    ("Pedro", 30, "Barcelona", "España", "Masculino"),
+    ("Luisa", 22, "Sevilla", "España", "Femenino"),
+    ("Ana", 25, "Madrid", "España", "Femenino"),
+    ("Pedro", 30, "Barcelona", "España", "Masculino"),
+    ("Luisa", 22, "Sevilla", "España", "Femenino")
+]
+for row in data:
+    arbol.insert("", "end", values=row)
+
+#Las scrollbar de la tabla
+barraX=tk.Scrollbar(frame2, orient="vertical", command=arbol.yview)
+barraX.pack(side="right", fill="y")
+arbol.config(yscrollcommand=barraX.set)
+
+'''barraY=tk.Scrollbar(frame2, orient="horizontal", command=arbol.xview)
+barraY.pack(side="bottom", fill="x")
+arbol.config(xscrollcommand=barraY.set)'''
+
+
 
 # Ejecuta la ventana
 ventana.mainloop()
